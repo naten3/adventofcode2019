3822034730b07d4a401664fba9ab7b376d1e27b9
Nate Nelles <nnelles1@gmail.com>
2020-01-27 17:38:12 -0600

    day 16 part 2

--- src/main/scala/adventofcode/Day16.scala
+++ src/main/scala/adventofcode/Day16.scala
@@ -8,20 +8,13 @@ object Day16 {
   def main(args: Array[String]): Unit = {
     val digits = Source.fromResource("day16.txt").getLines().mkString.toCharArray.map(_.asDigit);
 
-    println(part1(digits))
+    //println(part1(digits))
     println(part2(digits))
   }
 
   def part1(digits: Array[Int]): String = {
     val patterns = getPatterns(digits.length);
     (1 to 100).foldLeft(digits)((newDigits, _) => getOutputSequence(patterns, newDigits)).take(8).mkString
-  }
-
-  def part2(digits: Array[Int]): String = {
-    val skipNumber = digits.take(7).mkString.toInt
-    val newDigits = (1 to 1000).flatMap(_ => digits).toArray
-    val patterns = getPatterns(newDigits.length)
-    (1 to 100).foldLeft(newDigits)((nd, _) => getOutputSequence(patterns, nd)).slice(skipNumber,skipNumber + 8).mkString
   }
 
   def getOutputSequence(patterns: Array[Array[Int]], digits: Array[Int]): Array[Int] = {
@@ -38,6 +31,41 @@ object Day16 {
       val pattern = basePattern.flatMap( patternDigit => (1 to n).map(_ => patternDigit))
       (1 to length).map(m => pattern(m % pattern.length)).toArray
     }).toArray
+  }
+
+  // This only works if skipNumber > totalLength/2 
+  def part2(digits: Array[Int]): String = {
+    val skipNumber = digits.take(7).mkString.toInt
+    val repeatNumber = 10000
+    val totalLength = digits.length * repeatNumber
+    val resultLength = totalLength - skipNumber
+
+    val initial = new Array[Int](resultLength)
+    (0 until resultLength).foreach(i => {
+      val position = resultLength - 1 - i
+      val arrayValue = digits(digits.length - 1 - (i % digits.length))
+      initial(position) = arrayValue
+    })
+
+
+    def cycle(previousArray: Array[Int]) : Array[Int] = {
+      var previousSum = 0
+      val result = new Array[Int](resultLength)
+      (0 until resultLength).foreach(i => {
+        val position = resultLength - 1 - i
+        if (i == 0) {
+          result(position) = 0
+        } else {
+          previousSum = (previousSum + previousArray(position)) % 10
+          result(position) = previousSum
+        }
+      })
+      result
+    }
+
+    (1 to 100).foldLeft(initial)( (initial, _) => {
+      cycle(initial)
+    }).take(8).mkString
   }
 
 }
